17cdd056da6d330bca08f26224570c5dbd6f3390
Nadav Meroz <154219703+Nadav-gif@users.noreply.github.com>
2023-12-28 08:54:42 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -3,7 +3,7 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		int var1 = Integer.parseInt(var0[0]);
+		int var1 = Integer.parseInt(args[0]);
       		for(int var2 = 1; var2 <= var1; ++var2) {
          	if (var1 % var2 == 0) {
             		System.out.println(var2);
